fe6f1609131df70d3de9888bac40c7e0515ee791
Gabriel L. Oliveira <ciberglo@gmail.com>
2011-08-02 03:23:25 -0300

    Refactor to follow guidelines

--- testUNGDocs.py
+++ testUNGDocs.py
@@ -3,10 +3,11 @@ import unittest
 
 import urllib2
 
-SVG_CONTENT = urllib2.quote("<svg width='640' height='480' xmlns='http://www.w3.org/2000/svg'> \
+SVG_CONTENT = urllib2.quote("<svg width='640' height='480' \
+xmlns='http://www.w3.org/2000/svg'> \
 <g><title>Layer 1</title> \
-<ellipse ry='46' rx='47' id='svg_1' cy='93' cx='138' stroke-width='5' stroke='#000000' fill='#FF0000'/> \
-</g></svg>")
+<ellipse ry='46' rx='47' id='svg_1' cy='93' cx='138' stroke-width='5' \
+stroke='#000000' fill='#FF0000'/> </g></svg>")
 
 class TestUNGDocs(UNGTestMixin):
     """Tests related to UNG Docs"""
