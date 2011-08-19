ec46cb0a3add1617f6763cb3c54a305aaa376ebb
François Billioud <francois.billioud@tiolive.com>
2011-08-19 18:57:53 +0200

    little changes in the website appearance

--- UNGProject/index.php
+++ UNGProject/index.php
@@ -3,9 +3,9 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
         <title></title>
-        <script type="text/javascript"> function reloc(url) {window.location = url;}</script>
+        <script type="text/javascript"> function reloc(url) {window.location.replace(url);}</script>
     </head>
-    <body onload="reloc('mail.html');">
+    <body onload="reloc('ung.html');">
 
     </body>
 </html>
