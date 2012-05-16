73a1788f08fb398a274f4ca31024e366681fa82a
Sebastien Robin <seb@nexedi.com>
2012-05-16 15:14:28 +0200

    show how to render part of the user interface as cached gadgets

--- poc/renderjs/js/renderjs_poc.js
+++ poc/renderjs/js/renderjs_poc.js
@@ -1,7 +1,7 @@
 /* trigger when page is ready */
 $(document).ready(function (){
 
-  TabbularGadget.addNewTabGadget('gadget/nagivation.html','gadget-header',undefined);
+  TabbularGadget.addNewTabGadget('gadget/cached_navigation.html','gadget-header',undefined);
   TabbularGadget.addNewTabGadget('gadget/article.html','gadget-article',undefined);
 
 });
