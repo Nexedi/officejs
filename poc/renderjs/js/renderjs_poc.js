83da9717dbef3ac77e42beacbfe7a29fe80af89d
Sebastien Robin <seb@nexedi.com>
2012-05-18 17:53:24 +0200

    use latestt renderjs, update renderjs calls after api change

--- poc/renderjs/js/renderjs_poc.js
+++ poc/renderjs/js/renderjs_poc.js
@@ -1,7 +1,5 @@
 /* trigger when page is ready */
 $(document).ready(function (){
-
-  TabbularGadget.addNewTabGadget('gadget/cached_navigation.html','gadget-header',undefined);
-  TabbularGadget.addNewTabGadget('gadget/article.html','gadget-article',undefined);
-
+  RenderJs.TabbularGadget.addNewTabGadget('gadget-header', 'gadget/cached_navigation.html');
+  RenderJs.TabbularGadget.addNewTabGadget('gadget-article', 'gadget/article.html');
 });
