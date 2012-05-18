27df6f8cf5560698d6682e85708ebb935b380247
Sebastien Robin <seb@nexedi.com>
2012-05-18 18:11:12 +0200

    show how we can directly define gadget to define directly in index.html

--- poc/renderjs/js/renderjs_poc.js
+++ poc/renderjs/js/renderjs_poc.js
@@ -1,5 +1,7 @@
 /* trigger when page is ready */
 $(document).ready(function (){
-  RenderJs.TabbularGadget.addNewTabGadget('gadget-header', 'gadget/cached_navigation.html');
+  // Only bootstrap a div wich define a gadget
+  RenderJs.bootstrap($("#gadget-header"));
+  // Here we add gadget to an existing div with javascript code
   RenderJs.TabbularGadget.addNewTabGadget('gadget-article', 'gadget/article.html');
 });
