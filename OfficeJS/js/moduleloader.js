916cae479cda6ca3baafe08a622c67b9e2501ed9
Tristan Cavelier <tristan.cavelier@tiolive.com>
2012-05-24 18:03:38 +0200

    Make renderjs work with renderjs.

--- OfficeJS/js/moduleloader.js
+++ OfficeJS/js/moduleloader.js
@@ -1,17 +1,5 @@
 
-define ('OfficeJS',
-        ['LocalOrCookieStorage',
-         'jQuery',
-         'JIO',
-         'Base64',
-         'JIOStorages'],
-        function (LocalOrCookieStorage,
-                  jQuery,
-                  JIO,
-                  Base64) {
-            return {LocalOrCookieStorage: LocalOrCookieStorage,
-                    jQuery: jQuery,
-                    JIO: JIO,
-                    Base64: Base64,
-                    ich: window.ich};
+define ('OfficeJS',['order!jQuery','order!jStorage','order!RenderJS'],
+        function () {
+            return;
         });
--- OfficeJS/js/officejs.js
+++ OfficeJS/js/officejs.js
@@ -1,300 +1,257 @@
-// NEXEDI
-// Author: Tristan Cavelier <tristan.cavelier@tiolive.com>
-// Date: Wed May 16 14:31:08 2012
+(function () {
+    // Tools
+    var extend = function (o1,o2) {
+        var key; for (key in o2) { o1[key] = o2[key]; } return o1;
+    };
 
-require.config ({
-    paths: {
-        LocalOrCookieStorage: '../lib/jio/localorcookiestorage.min',
-        jQueryAPI: '../lib/jquery/jquery',
-        jQuery: '../js/jquery.requirejs_module',
-        JIO: '../src/jio',
-        Base64API: '../lib/base64/base64',
-        Base64: '../js/base64.requirejs_module',
-        JIOStorages: '../src/jio.storage',
-
-        OfficeJS: 'moduleloader'
-    }
-});
-require(['OfficeJS'],function (OJS) {
-    // globals
-    var JIO = OJS.JIO,
-    $ = OJS.jQuery,
-    Base64 = OJS.Base64,
-    ich = OJS.ich,
-    // some vars
-    text_editor_loaded_once = false,
-    current_hash = '',
-    ich_object = {DocumentList:[],CurrentFileName:''},
-    current_editor = null,
-    route_param = {},
-    // conf vars
-    routes = {
-        'default' : {template:'home'},
-        '/home' : {template:'home'},
-        '/about' : {template:'about'},
-        '/login' : {template:'login'},
-        '/doclist' : {template:'document_list'},
-        '/texteditor' : {
-            template:'text_editor',
-            onload:function(){
-                var intervalid;
-                if (!text_editor_loaded_once) {
-                    xinha_init();
-                    text_editor_loaded_once = true;
-                }
-                document.querySelector('#text_editor').style.display = 'block';
-                current_editor = 'xinha';
-                if (typeof route_param.fileName !== 'undefined') {
-                    intervalid = setInterval(function(){
-                        try {
-                            getCurrentEditor().getHTML();
-                        } catch (e) {
-                            return;
-                        }
-                        $('#input_fileName').attr(
-                            'value',route_param.fileName);
-                        OfficeJS.load();
-                        clearInterval(intervalid);
-                    },50);
-                }
-            },
-            onunload:function(){
-                document.querySelector('#text_editor').style.display = 'none';
-                ich_object.CurrentFileName = $('#input_fileName').attr('value');
-                current_editor = null;
-            },
-            onrestart: function(){
-                this.onload();
-            }
-        }
-    },
-    ////////////////////////////////////////////////////////////////////////////
-    // load current page
-    loadcurrentpage = function () {
-        var new_hash, params, i;
-        // get new hash
-        new_hash = location.hash.split('#');
-        if (typeof new_hash[1] !== "undefined") {
-            // new direction
-            new_hash = new_hash[1];
-            params = new_hash.split(':');
-            new_hash = params[0];
-            if (typeof routes[new_hash] === "undefined") {
-                return current_hash;
-            }
-            // set route_parameters
-            route_param = {};
-            for (i = 1; i < params.length; i += 1) {
-                var tmp = params[i].split('=');
-                route_param[tmp[0]] = tmp[1];
-            }
-        } else {
-            // default home
-            new_hash = 'default';
-        }
-        return new_hash;
-    },
-    // end load current page
-    ////////////////////////////////////////////////////////////////////////////
-    // Repaint main page
-    repaint = function () {
-        $('#main').html(ich[routes[current_hash].template](ich_object,true));
-    },
-    // end repaint main page
-    ////////////////////////////////////////////////////////////////////////////
-    // change page according to the event
-    hrefClicked = function () {
-        var new_hash = loadcurrentpage(), prev_hash = current_hash;
-        if (current_hash === '') {
-            current_hash = new_hash;
-            repaint();
-        }
-        if (routes[current_hash].template !== routes[new_hash].template) {
-            // check if it is necessary to repaint the page.
-            current_hash = new_hash;
-            if (typeof routes[prev_hash].onunload === 'function') {
-                routes[prev_hash].onunload();
-            }
-            repaint();
-            if (typeof routes[current_hash].onload === 'function') {
-                routes[current_hash].onload();
-            }
-        } else {
-            if (typeof routes[current_hash].onrestart === 'function') {
-                routes[current_hash].onrestart();
-            }
-        }
-    },
-    // end change page according to the event
-    ////////////////////////////////////////////////////////////////////////////
-    // get the currrent editor
-    getCurrentEditor = function () {
-        switch (current_editor) {
-        case 'xinha':
-            return xinha_editors['textEditor'];
-        case 'svg':
-            return null;
-        case 'calc':
-            return null;
-        default:
-            return null;
-        }
-    },
-    // end get the current editor
-    ////////////////////////////////////////////////////////////////////////////
-    // loading function
-    loading_object = {
-        spinstate: 0,savestate: 0,loadstate: 0,getliststate: 0,removestate: 0,
-        main: function (string){
-            if (this[string+'state'] === 0){
-                document.querySelector ('#loading_'+string).
-                    style.display = 'block';
-            }
-            this[string+'state'] ++;
-        },
-        end_main: function (string){
-            if (this[string+'state']>0) {
-                this[string+'state']--;
-            }
-            if (this[string+'state']===0){
-                document.querySelector ('#loading_'+string).
-                    style.display = 'none';
-            }
-        },
-        spin:function(){this.main('spin');},
-        save:function(){this.main('save');this.spin();},
-        load:function(){this.main('load');this.spin();},
-        getlist:function(){this.main('getlist');this.spin();},
-        remove:function(){this.main('remove');this.spin();},
-        end_spin:function(){this.end_main('spin');},
-        end_save:function(){this.end_main('save');this.end_spin();},
-        end_load:function(){this.end_main('load');this.end_spin();},
-        end_getlist:function(){this.end_main('getlist');this.end_spin();},
-        end_remove:function(){this.end_main('remove');this.end_spin();}
-    }
-    // end loading function
-    ////////////////////////////////////////////////////////////////////////////
-
-    // repaint the page
-    hrefClicked();
-
+    /**
+     * OfficeJS Object
+     */
     window.OfficeJS = (function () {
-        var publ = {}, priv = {};
+        var that = {}, priv = {};
+        // Attributes //
+        priv.preference_object = {
+            default_app:'login',
+            login:'login',
+            topnavbar:'topnavbar',
+            leftnavbar:'leftnavbar',
+            textEditor:'elrte'
+        };
+        priv.app_object = {
+            login: {
+                path:'component/login.html',
+                gadgetid:'page-content',
+                getContent: function () {
+                    var tmp = {
+                        userName: 'NoName',
+                        password: 'NoPwd'
+                    };
+                    // NOTE : stringify or not ?
+                    return JSON.stringify (tmp);
+                }
+            },
+            elrte: {
+                path:'component/elrte.html',
+                element:'#elrte_editor',
+                getContent: function () {
+                    $(this.element).elrte('updateSource');
+                    return $(this.element).elrte('val');
+                },
+                onload: function () {},
+                onunload: function () {}
+            }
+        };
+        priv.data_object = {
+            documentList:[]
+        };
+        priv.loading_object = {
+            spinstate: 0,
+            savestate: 0,
+            loadstate: 0,
+            getliststate: 0,
+            removestate: 0,
+            main: function (string){
+                if (this[string+'state'] === 0){
+                    document.querySelector ('#loading_'+string).
+                        style.display = 'block';
+                }
+                this[string+'state'] ++;
+            },
+            end_main: function (string){
+                if (this[string+'state']>0) {
+                    this[string+'state']--;
+                }
+                if (this[string+'state']===0){
+                    document.querySelector ('#loading_'+string).
+                        style.display = 'none';
+                }
+            },
+            spin:function(){this.main('spin');},
+            save:function(){this.main('save');this.spin();},
+            load:function(){this.main('load');this.spin();},
+            getlist:function(){this.main('getlist');this.spin();},
+            remove:function(){this.main('remove');this.spin();},
+            end_spin:function(){this.end_main('spin');},
+            end_save:function(){this.end_main('save');this.end_spin();},
+            end_load:function(){this.end_main('load');this.end_spin();},
+            end_getlist:function(){this.end_main('getlist');this.end_spin();},
+            end_remove:function(){this.end_main('remove');this.end_spin();}
+        };
+
+        // Initializer //
+        priv.init = function() {
+        };
+
+        // Methods //
+        /**
+         * @method getPreference
+         * @param  {string} key The preference
+         * @return {object} a clone of the preference object
+         */
+        that.getPreference = function (key) {
+            return priv.preference_object[key];
+        };
+
+        /**
+         * @method jioIsSet
+         * @return {boolean} true if jio is set else false.
+         */
         priv.jioIsSet = function () {
             return (typeof priv.jio === 'object');
         };
-        publ.setJio = function () {
+
+        /**
+         * @method getContentOf
+         * @param  {string} app The application name
+         * @return {string} The content of the application, or null.
+         */
+        that.getContentOf = function (app) {
+            if (priv.app_object[app] &&
+                typeof priv.app_object[app].getContent !== 'undefined') {
+                return priv.app_object[app].getContent();
+            }
+            return null;
+        };
+
+        /**
+         * @method getPathOf
+         * @param  {string} app The application name
+         * @return {string} The path of the application component, or null.
+         */
+        that.getPathOf = function (app) {
+            if (priv.app_object[app]) {
+                return priv.app_object[app].path;
+            }
+            return null;
+        };
+
+        /**
+         * @method setJio
+         * @param {object} storage The storage informations
+         * @param {object} applicant The applicant informations
+         */
+        that.setJio = function (storage,applicant) {
             if (priv.jioIsSet()) {
                 alert ('Jio already set.');
                 return;
             }
-            alert ($('#input_json_storage').attr('value'));
             // if there is not any jio created
-            priv.jio = JIO.createNew (
-                JSON.parse ( $('#input_json_storage').attr('value') ),
-                JSON.parse ( $('#input_json_applicant').attr('value') )
-            );
-            publ.getlist();
+            priv.jio = JIO.createNew (storage,applicant);
+            that.getList();
         };
-        publ.save = function () {
-            var filename, filecontent;
+
+        /**
+         * Returns the array list in priv.data_object
+         * @method getList
+         */
+        that.getList = function () {
             if (!priv.jioIsSet()) {
-                alert ('No Jio set yet.');
+                console.error ('No Jio set yet.');
                 return;
             }
-            loading_object.save();
-            filename = $('#input_fileName').attr('value');
-            filecontent = getCurrentEditor().getHTML();
-            priv.jio.saveDocument({
-                'fileName':filename,
-                'fileContent':filecontent,
-                'callback':function (result){
-                    if (result.status === 'fail') {
-                        console.error (result.error);
-                    }
-                    loading_object.end_save();
-                    publ.getlist();
-                }
-            });
-        };
-        publ.load = function () {
-            var filename;
-            if (!priv.jioIsSet()) {
-                alert ('No Jio set yet.');
-                return;
-            }
-            loading_object.load();
-            filename = $('#input_fileName').attr('value');
-            priv.jio.loadDocument({
-                'fileName':filename,
-                'maxtries':3,
-                'callback':function (result){
-                    if (result.return_value.fileName) {
-                        getCurrentEditor().setHTML(
-                            result.return_value.fileContent);
-                    } else {
-                        console.error (result.error);
-                    }
-                    loading_object.end_load();
-                }
-            });
-        };
-        publ.remove = function () {
-            var filename;
-            if (!priv.jioIsSet()) {
-                alert ('No Jio set yet.');
-                return;
-            }
-            loading_object.remove();
-            filename = $('#input_fileName').attr('value');
-            priv.jio.removeDocument({
-                'fileName':filename,
-                'callback':function (result) {
-                    if (result.status === 'fail') {
-                        console.error (result.error);
-                    }
-                    loading_object.end_remove();
-                    publ.getlist();
-                }
-            });
-        };
-        publ.getlist = function () {
-            if (!priv.jioIsSet()) {
-                alert ('No Jio set yet.');
-                return;
-            }
-            loading_object.getlist();
+            priv.loading_object.getlist();
             priv.jio.getDocumentList({
                 'maxtries':3,
                 'callback':function (result) {
                     if (result.status === 'done') {
-                        var htmlString = '', i, document_array = [];
-                        for (i = 0; i < result.return_value.length; i += 1) {
-                            htmlString += '<li><a href="#/texteditor:fileName='+
-                                result.return_value[i].fileName + '">\n' +
-                                result.return_value[i].fileName;
-                            result.return_value[i].creationDate =
-                                (new Date(result.return_value[i].creationDate)).
-                                toLocaleString();
-                            result.return_value[i].lastModified =
-                                (new Date(result.return_value[i].lastModified)).
-                                toLocaleString();
-                            document_array.push (result.return_value[i]);
-                            htmlString += '</a></li>\n';
-                        }
-                        if (htmlString === '') {
-                            htmlString = 'No document';
-                        }
-                        ich_object.DocumentList = document_array;
-                        document.querySelector ('#document_list').
-                            innerHTML = htmlString;
-                        loading_object.end_getlist();
+                        priv.data_object = result.return_value;
+                    } else {
+                        console.error (result.message);
                     }
+                    priv.loading_object.end_getlist();
+                    // TODO : show list somewhere
                 }
             });
         };
-        return publ;
-    }());
-    // When someone clicks on a <a href..>..</a>, the interface may change.
-    $(window).bind('hashchange', hrefClicked);
-});
+
+        /**
+         * Saves the document.
+         * @method save
+         * @param  {string} name The document name.
+         * @param  {string} content The content of the document.
+         */
+        that.save = function (name, content) {
+            if (!priv.jioIsSet()) {
+                console.error ('No Jio set yet.');
+                return;
+            }
+            priv.loading_object.save();
+            priv.jio.saveDocument({
+                'fileName':name,
+                'fileContent':content,
+                'callback':function (result) {
+                    if (result.status === 'fail') {
+                        console.error (result.message);
+                    }
+                    priv.loading_object.end_save();
+                    that.getList();
+                }
+            });
+        };
+
+        /**
+         * Loads a document.
+         * @method load
+         * @param  {string} name The document name.
+         */
+        that.load = function (name) {
+            if (!priv.jioIsSet()) {
+                console.error ('No Jio set yet.');
+                return;
+            }
+            priv.loading_object.load();
+            priv.jio.loadDocument({
+                'fileName':name,
+                'maxtries':3,
+                'callback':function (result) {
+                    if (result.status === 'fail') {
+                        console.error (result.message);
+                    }
+                    priv.loading_object.end_load();
+                    // TODO : show content somewhere
+                }
+            });
+        };
+
+        /**
+         * Removes a document.
+         * @method remove
+         * @param  {string} name The document name.
+         */
+        that.remove = function (name) {
+            if (!priv.jioIsSet()) {
+                console.error ('No Jio set yet.');
+                return;
+            }
+            priv.loading_object.remove();
+            priv.jio.removeDocument({
+                'fileName':name,
+                'callback':function (result) {
+                    if (result.status === 'fail') {
+                        console.error (result.message);
+                    }
+                    priv.loading_object.end_remove();
+                    that.getList();
+                }
+            });
+        };
+
+        // End of class //
+        priv.init();
+        return that;
+    }());                       // end OfficeJS
+
+    // show gadgets
+    TabbularGadget.addNewTabGadget(
+        'component/top_nav_bar.html',
+        'page-top_nav_bar',
+        undefined);
+    TabbularGadget.addNewTabGadget(
+        'component/left_nav_bar.html',
+        'page-left_nav_bar',
+        undefined);
+    TabbularGadget.addNewTabGadget(
+        'component/login.html',
+        'page-content',
+        undefined);
+}());
