c1f9edc9c5dddbd99d9a7641dab92cdf4e7b40be
Thibaut Frain <thibaut.frain@tiolive.com>
2013-10-04 13:33:50 +0200

    node_modules parent directory is now officejs

--- config.mk
+++ config.mk
@@ -2,10 +2,10 @@
 VERSION = 1.9.0
 NAME = officejs
 
-JSLINT = /home/romain/devel/nowm/gidzit/node_modules/.bin/jslint
-LESSC = /home/romain/devel/nowm/gidzit/node_modules/.bin/lessc
-UGLIFYJS = ~/node_modules/.bin/uglifyjs
-JISON = /home/romain/devel/nowm/gidzit/node_modules/.bin/jison
-PHANTOMJS = xvfb-run phantomjs
-MINIFY = ~/node_modules/.bin/minify
+JSLINT = ./node_modules/.bin/jslint
+LESSC = ./node_modules/.bin/lessc
+UGLIFYJS = ./minify/node_modules/.bin/uglifyjs
+JISON = ./node_modules/.bin/jison
+PHANTOMJS = ./node_modules/.bin/phantomjs
+MINIFY = ./node_modules/.bin/minify
 PYTHON = /usr/bin/python2
