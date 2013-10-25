8b5ed49af294a52e85194a5ab5b3149329437f2b
Thibaut Frain <thibaut.frain@tiolive.com>
2013-10-25 14:28:11 +0200

    Added bootstrap wysiwyg editor

---
+++ src/js/gadget_bootstrap_wysiwyg.js
@@ -0,0 +1,28 @@
+/*global window, jQuery, rJS*/
+"use strict";
+
+(function (window, $, rJS) {
+
+  function init() {
+    this.editor = $('#editor');
+    this.editor.wysiwyg();
+  }
+
+  rJS(window).declareMethod('getContent', function () {
+    return rJS(this).editor.html();
+  })
+
+    .declareMethod('setContent', function (content) {
+      console.log('setcontent:' + content);
+      rJS(this).editor.html(content);
+    })
+
+    .declareMethod('clearContent', function () {
+      rJS(this).editor.html('');
+    })
+
+    .ready(function () {
+      init.apply(rJS(this));
+    });
+
+}(window, jQuery, rJS));
