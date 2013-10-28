8d556d5f8fde87661914fbad267a903cdf84bd55
Thibaut Frain <thibaut.frain@tiolive.com>
2013-10-28 14:11:09 +0100

    webodf minimal integration

---
+++ src/js/gadget_webodf.js
@@ -0,0 +1,11 @@
+/*global window, jQuery, rJS, odf*/
+"use strict";
+
+(function (window, $, rJS, odf) {
+  rJS(window).ready(function () {
+    var gadget = rJS(this);
+    gadget.odfElem = $('#odf')[0];
+    gadget.odfCanvas = odf.OdfCanvas(gadget.odfElem);
+  });
+
+}(window, jQuery, rJS, odf));
