91feccb63e5ab6ffaca0ac6e70426712b0d47a54
Marco Mariani <marco.mariani@nexedi.com>
2014-02-20 14:29:47 +0100

    svg-edit: configure link to extensions and images, jslint

--- src/js/gadget_svgedit.js
+++ src/js/gadget_svgedit.js
@@ -1,20 +1,23 @@
-/*global window, jQuery, rJS, svgCanvas */
-/*jslint evil: true*/
+/*jslint indent: 2 */
+/*global window, jQuery, rJS, svgCanvas, svgEditor */
 "use strict";
 
 (function (window, rJS) {
 
+  svgEditor.setConfig({
+    extPath: '../lib/svg-edit-2.6/extensions/',
+    imgPath: '../lib/svg-edit-2.6/images/'
+  });
+
   rJS(window).declareMethod('getContent', function () {
     console.log(window.svgCanvas.getSvgString());
     return window.svgCanvas.getSvgString();
-  })
-  
-    .declareMethod('setContent', function (content) {
+  }).
+    declareMethod('setContent', function (content) {
       console.log(window.content);
       window.svgCanvas.setSvgString(content);
-    })
-  
-    .declareMethod('clearContent', function () {
+    }).
+    declareMethod('clearContent', function () {
       window.svgCanvas.clear();
     });
 
