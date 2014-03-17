5ee43a60b5b4ae31ea8035b9bcf7a475e941c3f2
Marco Mariani <marco.mariani@nexedi.com>
2014-03-17 14:25:17 +0100

    bootstrap-wysiwyg: fixed font menu

--- src/js/gadget_bootstrap_wysiwyg.js
+++ src/js/gadget_bootstrap_wysiwyg.js
@@ -4,24 +4,65 @@
 
 (function (window, $, rJS) {
 
-  function init() {
-    this.editor = $('#editor');
-    this.editor.wysiwyg();
+  function initToolbarBootstrapBindings() {
+    var fonts = [
+      'Serif', 'Sans', 'Arial', 'Arial Black', 'Courier', 'Courier New', 'Comic Sans MS', 'Helvetica',
+      'Impact', 'Lucida Grande', 'Lucida Sans', 'Tahoma', 'Times', 'Times New Roman', 'Verdana'
+    ], fontTarget = $('[title=Font]').siblings('.dropdown-menu');
+
+    $.each(fonts, function (idx, fontName) {
+      /*jslint unparam: true */
+      fontTarget.append($('<li><a data-edit="fontName ' + fontName + '" style="font-family:\'' + fontName + '\'">' + fontName + '</a></li>'));
+    });
+
+    $('.dropdown-menu input').
+      click(function () {
+        return false;
+      }).
+      change(function () {
+        $(this).parent('.dropdown-menu').siblings('.dropdown-toggle').dropdown('toggle');
+      }).
+      keydown('esc', function () {
+        this.value = '';
+        $(this).change();
+      });
+
+    $('[data-role=magic-overlay]').each(function () {
+      var overlay = $(this), target = $(overlay.data('target'));
+      overlay.css('opacity', 0).css('position', 'absolute').offset(target.offset()).width(target.outerWidth()).height(target.outerHeight());
+    });
   }
 
-  rJS(window).declareMethod('getContent', function () {
-    return rJS(this).editor.html();
-  })
+  function showErrorAlert(reason, detail) {
+    var msg = '';
+    if (reason === 'unsupported-file-type') {
+      msg = "Unsupported format " + detail;
+    } else {
+      console.log("error uploading file", reason, detail);
+    }
+    $('<div class="alert"> <button type="button" class="close" data-dismiss="alert">&times;</button>' +
+      '<strong>File upload error</strong> ' + msg + ' </div>').prependTo('#alerts');
+  }
 
+  function init() {
+    this.editor = $('#editor');
+    initToolbarBootstrapBindings();
+    this.editor.wysiwyg({
+      fileUploadError: showErrorAlert
+    });
+  }
+
+  rJS(window).
+    declareMethod('getContent', function () {
+      return rJS(this).editor.html();
+    })
     .declareMethod('setContent', function (content) {
       //console.log('setcontent:' + content);
       rJS(this).editor.html(content);
     })
-
     .declareMethod('clearContent', function () {
       rJS(this).editor.html('');
     })
-
     .ready(function (g) {
       init.apply(g);
     });
